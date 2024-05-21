3621a4cf978bcc36d0464695a283b708282f86ae
Firas Khalil Khana <firasuke@gmail.com>
2024-05-21 22:54:18 +0300

    Improve sidebar layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,11 @@ export default defineConfig({
 	integrations: [
 		starlight({
 			title: 'glaucus',
+			description: 'A simple and lightweight Linux® distribution based on musl libc and toybox',
+			favicon: '/src/assets/glaucus-logo.svg',
 			logo: {
         src: './src/assets/glaucus-logo.svg',
       },
-			description: 'A simple and lightweight Linux® distribution based on musl libc and toybox',
 			social: {
 				discord: 'https://discord.gg/nDKNmNc',
 				reddit: 'https://www.reddit.com/r/glaucus',
@@ -16,7 +17,42 @@ export default defineConfig({
 				twitter: 'https://twitter.com/glaucuslinux',
 				youtube: 'https://www.youtube.com/@glaucuslinux',
 			},
-			favicon: '/favicon.ico',
+			sidebar: [
+        {
+          label: "Bootstrap",
+          link: "/bootstrap",
+        },
+        {
+          label: "cerata",
+          collapsed: true,
+          autogenerate: { directory: "cerata" },
+        },
+        {
+          label: "History",
+          link: "/history",
+        },
+        {
+          label: "Install",
+          link: "/install",
+        },
+        {
+          label: "Naming Convention",
+          link: "/naming-convention",
+        },
+        {
+          label: "Package Management",
+          link: "/package-management",
+        },
+        {
+          label: "RFCs",
+          collapsed: true,
+          autogenerate: { directory: "RFCs" },
+        },
+        {
+          label: "TODO",
+          link: "/todo",
+        },
+      ],
 		}),
 	],
 });
